185a1215f50386133eaebee1de6ce4a38e72d1f5
tyroPatel <tyresecook25@yahoo.com>
2023-12-11 23:18:22 -0600

    [dev] Fixed text issue in SVG & improved Finalize UX

--- js/cozzi.js
+++ js/cozzi.js
@@ -2,7 +2,7 @@ inputs = ['name','email','cell'];
 cards = [];
 class Card{
 	constructor(name,email,cell) {
-		this.inputValues = [name,email,cell];
+		this.inputValues = {'name':name, 'email':email,'cell':cell};
 		this.card;
 		this.textEle = [];
 		this.createSVG();
@@ -19,44 +19,57 @@ class Card{
 	}
 	//Sets the text in SVG element to initiating variables
 	setText(svgObj){
-		var tspanEles = svgObj.contentDocument.querySelectorAll("text tspan>tspan");
-
+		var tspanEles = svgObj.contentDocument.querySelectorAll("text tspan");
+		console.log(tspanEles);
 		tspanEles.forEach((ele,i) => {
-			ele.innerHTML = this.inputValues[i];
+			console.log(ele.parentElement.id);
+			ele.innerHTML = this.inputValues[ele.parentElement.id];
 
 			//centers the updated text if grandparent's id has rel in it 
+			console.log($(ele).parent());
 			if ($(ele).parent().parent().attr('id').indexOf("rel") >-1) {
 				var rect = svgObj.contentDocument.querySelector("rect#positioner");
 				var eleWidth = ele.textLength.baseVal.value;
 				var newXPos = ((parseFloat($(rect).attr('width')) - eleWidth) / 2) + parseFloat($(rect).attr('x'));
 				$(ele).attr('x',newXPos);
 			}
+			
 		});
 	}
-	display(){					
+	display(){		
 		$("#svg-grid").append(this.card);
 	}
 }
+
 $("#populate").on('click',function(){
+	console.log($("#svg-grid"));
 	//Loop a number of times
 	for(i=0; i<$("#number").val(); i++){ 
 		newDiv = document.createElement("div");
 		$(newDiv).attr('design', $("#card-design").val());
 
-		//creates a fixed number of input tags
+		//creates a fixed number of input tags in newDiv
 		for(x=0;x < inputs.length; x++){
 			newObj1 = document.createElement("input");
 			newObj1.name = [inputs[x]];
 			newObj1.placeholder = [inputs[x]]
 			newDiv.appendChild(newObj1);
 		}
-		$("#svg-grid").append(newDiv);
+		//Take newdiv and send it to the front of the list
+		$("#svg-grid").prepend(newDiv);
 	}
 });
+//Creates the cards if they dont exists
 $("#finalize").on('click',function(){
 	cards = []; 
-	$("#svg-grid div").each(function(e){
+	
+	if ($("#svg-grid object").length > 0)
+		$("#svg-grid object").remove();
+	
+	$("#svg-grid div[design]").each(function(e){
+
 		var card = new Card($(this).find("input[name=name]")[0].value,$(this).find("input[name=email]")[0].value,$(this).find("input[name=cell]")[0].value);
 		cards.push(card)
 	});
+	
 });
